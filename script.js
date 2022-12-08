23c25b08dbfc17ad9e03a5f86b0ea22fae1f605f
Kenneth Huddleston <kenneth.j.huddleston@gmail.com>
2022-12-08 13:21:30 -0600

    Reworks functions to allow for user grid selection

--- script.js
+++ script.js
@@ -1,25 +1,27 @@
-const squaresContainer = document.querySelector("#squaresContainer");
+let changeGridBtn = document.querySelector("#changeGridBtn");
+let squaresContainer = document.querySelector("#squaresContainer");
+let squares = document.querySelectorAll(".square");
+let numberOfSquares = 256;
+
 const createDivs = (squares) => {
+    squaresContainer.remove();
+    squaresContainer = document.createElement('div');
+    squaresContainer.id = "squaresContainer";
+    document.body.appendChild(squaresContainer);
     let squareRoot = Math.sqrt(squares);
     squaresContainer.style.gridTemplateColumns = `repeat(${squareRoot}, 1fr)`;
     for (i = 1; i <= squares; i++) {
         let currentSquare = document.createElement("div");
         currentSquare.id = `square${i}`;
         currentSquare.className = 'square';
+        currentSquare.addEventListener("mouseover", () => currentSquare.style.backgroundColor = 'black');
         squaresContainer.appendChild(currentSquare);
     }
-}
-createDivs(4096);
+};
 
-const squares = document.querySelectorAll(".square");
+createDivs(numberOfSquares);
 
-const changeBackgroundColor = (currentSquare) => {
-    currentSquare.style.backgroundColor = 'black';
-}
-
-for (i = 0; i < squares.length; i++) {
-    let currentSquare = squares[i];
-    currentSquare.addEventListener("mouseover", () => {
-        changeBackgroundColor(currentSquare)
-    });
-}
+changeGridBtn.addEventListener("click", () => {
+    numberOfSquares = prompt('How many squares');
+    createDivs(numberOfSquares);
+});
